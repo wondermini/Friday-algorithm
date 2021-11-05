3a613dc80915e8599989e33566d35f90cd4ded7d
Wondermini <wondermini@gmail.com>
2021-11-05 14:40:24 +0900

    문제들을  별도 파일로 분리 1008번 추가

--- Friday-algorithm/Friday-algorithm-week1/main.swift
+++ Friday-algorithm/Friday-algorithm-week1/main.swift
@@ -5,9 +5,10 @@
 //  Created by JUNGMIN KIM on 2021/11/05.
 //
 
-//https://www.acmicpc.net/problem/1000
 import Foundation
 
-let input=readLine()!
-let inputArr=input.split(separator: " ")
-print(Int(inputArr[0])! + Int(inputArr[1])!)
+p1000()
+p1008()
+
+
+
