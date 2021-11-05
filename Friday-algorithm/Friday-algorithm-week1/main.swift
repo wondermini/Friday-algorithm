90d47c425893fb0056d74b15475b88cc51506bcd
Wondermini <wondermini@gmail.com>
2021-11-05 11:36:10 +0900

    https://www.acmicpc.net/problem/1000 문제 풀이 추가

---
+++ Friday-algorithm/Friday-algorithm-week1/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Friday-algorithm
+//
+//  Created by JUNGMIN KIM on 2021/11/05.
+//
+
+//https://www.acmicpc.net/problem/1000
+import Foundation
+
+let input=readLine()!
+let inputArr=input.split(separator: " ")
+print(Int(inputArr[0])! + Int(inputArr[1])!)
